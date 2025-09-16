99799444bca869cdbeb5d9ceb9820abade8175ff
mehul <manishbhoixd@gmail.com>
2025-09-16 10:27:13 +0530

    :recurring payment info

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import LoginPage from "./pages/loginPage";
 import SignupPage from "./pages/signupPage";
 import DashBoard from "./pages/dashBoard";
 import HomePage from "./pages/HomePage";
+import RecurringPaymentsAlerts from "./pages/RecurringPayment";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
           <Route path="/login" element={<LoginPage />}></Route>
           <Route path="/signup" element={<SignupPage />}></Route>
           <Route path="/dashboard" element={<DashBoard />}></Route>
+          <Route path="/payment" element={<RecurringPaymentsAlerts/>}></Route>
           <Route path="*" element={<HomePage />}></Route>
         </Routes>
       </BrowserRouter>
